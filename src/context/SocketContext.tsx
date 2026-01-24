404dea57c781e008d2756f8f535179b5feddc6bc
Will Kim <rlagmlckd@gmail.com>
2026-01-24 18:49:12 +0900

    feat: .env에 변수값을 추가할 수 있도록 셋팅

--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:3000";
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:3000";
 
 interface SocketContextType {
   socket: Socket | null;
